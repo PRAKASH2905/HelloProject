5d6b735da008c388db83b2ad69417f54a636382e
PRAKASH2905 <prakash.qa69@gmail.com>
2019-03-16 21:39:07 +0530

    version7

--- src/test/java/com/qaagility/controller/CalcTest.java
+++ src/test/java/com/qaagility/controller/CalcTest.java
@@ -33,8 +33,8 @@ public class CalcTest {
 
     @test
 	public void test111() {
-calcumul pp=new calcumul();
-calculator cl=new calculator();	
+Calcumul pp=new Calcumul();
+Calculator cl=new Calculator();	
 int res=pp.mul();
 int res1=cl.add();
        assertEquals("Result - Fail", 9, 9);
